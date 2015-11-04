d37a57ba350cc2d2d7004d4e963622dcb7e68cc7
Olivier Cros <cros@ece.fr>
2015-11-04 17:25:48 +0100

    Mise à jour du numéro de version

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,3 +1,3 @@
 <div id="footer">
-	<p>Artemis v1.8.0.3 - by Olivier Cros & Lucie Teilhac at ECE Paris - Feedback : cros@ece.fr</p>
+	<p>Artemis v1.8.0.4 - by Olivier Cros & Lucie Teilhac at ECE Paris - Feedback : cros@ece.fr</p>
 </div>
