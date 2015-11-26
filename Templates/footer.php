63f71baf5d250656417a97afa20e4f7f10dcbf7d
Olivier Cros <cros@ece.fr>
2015-11-26 15:54:51 +0100

    Intégration de la mise à jour du grapher

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,3 +1,3 @@
 <div id="footer">
-	<p>Artemis v1.8.1 - by Olivier Cros - ECE Paris - Feedback : cros@ece.fr - <a onclick="loadContent('credits')">Cr&eacute;dits</a></p>
+	<p>Artemis v1.9.0 - Feedback : cros@ece.fr - <a onclick="loadContent('credits')">Cr&eacute;dits</a></p>
 </div>
